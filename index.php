3d11a34c7f2f0c89a77854e52ad7c7c14689289a
Alberto <alberto.abate87@gmail.com>
2020-12-15 18:15:51 +0100

    esercizio completo

--- index.php
+++ index.php
@@ -8,18 +8,44 @@
 <body>
     
     <?php
+     $name = $_GET['name'];
+     $email = $_GET['email'];
+     $age = $_GET['age'];
+
+    // esercizio numero 1
+    // ?name=alberto
+    if ( empty($name)  ) {
+        echo "Errore, nessun parametro per name";
+    }
+    elseif (strlen($name) > 3) {
+        echo "accesso riuscito";
+    }
+    else {
+        echo "accesso negato";
+    }
     // esercizio numero 2
     // ?email=alberto.abate87@gmail.com
-    if ( empty($_GET ["email"]) ) {
-        echo "Errore, nessun paramentro per l'indirizzo email";
+    if ( empty($email) ) {
+        echo "Errore, nessun parametro per l'indirizzo email";
     }
-    elseif ( strpos($_GET["email"], '@') === false || strpos($_GET["email"], '.') === false) {
+    elseif ( strpos($email, '@') === false || strpos($email, '.') === false) {
         echo "Accesso negato";
     }
     else {
         echo "Accesso riuscito";
     }
+    // esercizio numero 3
+    // ?age=33
+    if (is_numeric ($age )) {
+        echo "accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    }
 
+    var_dump($name);
+    var_dump($email);
+    var_dump($age);
+    
     ?>
 
 </body>
